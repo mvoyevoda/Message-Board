12a46dc8d99f745fa315625558d950c87aa4b21b
Maxim Voyevoda <maximvoyevoda@gmail.com>
2023-05-16 15:31:53 -0400

    in progress

--- register.php
+++ register.php
@@ -2,6 +2,9 @@
 // Include db connection file
 require_once "db_connection.php";
 
+//Resume session
+session_start();
+
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = $email = "";
 $username_err = $password_err = $confirm_password_err = $email_err = "";
@@ -29,8 +32,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 if ($stmt->num_rows == 1) {
                     $username_err = "This username is already taken.";
-                    $_SESSION['errors'] += " " . $username_err;
-                    header("Location: " . $_SERVER["HTTP_REFERER"]);
+                    $_SESSION['errors'] = $username_err . "<br>";
+                    // header("Location: " . $_SERVER["HTTP_REFERER"]);
                 } else {
                     $username = trim($_POST["username"]);
                 }
@@ -62,8 +65,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 if ($stmt->num_rows == 1) {
                     $email_err = "This email is already registered.";
-                    $_SESSION['errors'] += " " . $email_err;
-                    header("Location: " . $_SERVER["HTTP_REFERER"]);
+                    $_SESSION['errors'] .= $email_err . "<br>";
+                    // header("Location: " . $_SERVER["HTTP_REFERER"]);
                 } else {
                     $email = trim($_POST["email"]);
                 }
@@ -77,6 +80,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate password
     if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter a password.";
+        $_SESSION['errors'] .= $password_err . "<br>";
     } elseif (strlen(trim($_POST["password"])) < 1) {
         // $password_err = "Password must have at least 6 characters.";
     } else {
@@ -86,12 +90,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate confirm password
     if (empty(trim($_POST["confirm_password"]))) {
         $confirm_password_err = "Please confirm password.";
+        $_SESSION['errors'] .= $confirm_password_err . "<br>";
     } else {
         $confirm_password = trim($_POST["confirm_password"]);
         if (($password != $confirm_password)) {
             $confirm_password_err = "Password did not match.";
+            $_SESSION['errors'] .= $confirm_password_err . "<br>";
         }
     }
+
+    // if (!empty($username_err) || !empty($email_err)){
+    //     $_SESSION['errors'] =  "Username and/or Email is already taken";
+    // }
 
     // Check input errors before inserting in database
     if (empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)) {
@@ -124,3 +134,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-euiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Message Board</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+
+        <header>
+            <h2 class="logo">Message Board</h2>
+            <nav class="navigation">
+                <a href="index.php">Home</a>
+                <a href="about.php">About</a>
+                <button class="btnLogin-popup">Login</button>
+            </nav>
+        </header>
+
+        <div class="register-box">
+            <h2>Registration</h2>
+            <form action="register.php" method="POST">
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="person"></ion-icon></span>
+                    <input type="text" name="username" required>
+                    <label>Username</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <input type="email" name="email" required>
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" name="password" required>
+                    <label>Password</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" name="confirm_password" required>
+                    <label>Confirm Password</label>
+                </div>
+                <button type="submit" class="btn">Register</button>
+                <div class="login-register">
+                    <p>Already have an account? <a href="login.php" class="login-link">Login</a></p>
+                </div>
+            </form>
+        </div>
+
+        <?php 
+        echo (empty($_SESSION['errors']) ? "NO ERRORS" : $_SESSION['errors']); 
+        $_SESSION['errors'] = "";
+        ?>
+
+        <script src="script.js"></script>
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </body>
+</html>
