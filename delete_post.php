1891b8f0e197dc6c119f65d42d8403bfaa115ad2
Maxim Voyevoda <maximvoyevoda@gmail.com>
2023-05-18 17:13:58 -0400

    fixed delete post when comments exist

--- delete_post.php
+++ delete_post.php
@@ -6,6 +6,24 @@ require_once "db_connection.php";
 if (isset($_POST["postid"])) {
     $postid = $_POST["postid"];
 
+    //First delete all comments from post
+
+    // Prepare the SQL statement
+    $stmt = $conn->prepare("DELETE FROM comments WHERE postid = ?");
+
+    // Bind the parameter
+    $stmt->bind_param("i", $postid);
+
+    // Execute the query
+    if ($stmt->execute()) {
+        echo "Comments deleted successfully!";
+    } else {
+        echo "Failed to delete comments. Please try again.";
+    }
+
+    // Close the statement
+    $stmt->close();    
+
     // Prepare the SQL statement
     $stmt = $conn->prepare("DELETE FROM posts WHERE postid = ?");
 
